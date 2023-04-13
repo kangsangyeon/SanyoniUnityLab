6663455379445c821f37d1bfa557fdbcc6bbd4c8
DESKTOP-KSANGYE\KOCCA21 <ksyline1997@gmail.com>
2023-04-13 17:30:50 +0900

    UniRxExample 씬을 추가합니다.

---
+++ Assets/@MyProject/Examples/UniRxExample/DetectDoubleClick.cs
@@ -0,0 +1,16 @@
+using System;
+using UniRx;
+using UnityEngine;
+
+public class DetectDoubleClick : MonoBehaviour
+{
+    private void Start()
+    {
+        var clickStream = Observable.EveryUpdate()
+            .Where(_ => Input.GetMouseButtonDown(0));
+
+        clickStream.Buffer(clickStream.Throttle(TimeSpan.FromMilliseconds(250)))
+            .Where(xs => xs.Count >= 2)
+            .Subscribe(xs => Debug.Log("DoubleClick Detected! Count:" + xs.Count));
+    }
+}
